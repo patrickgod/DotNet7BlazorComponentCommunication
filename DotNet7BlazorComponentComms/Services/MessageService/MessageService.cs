09be562b65490bb329b4e897e500bf8f77c4b8f5
Patrick <patrick@godknows.de>
2023-01-30 20:52:36 +0100

    Add project files.

---
+++ DotNet7BlazorComponentComms/Services/MessageService/MessageService.cs
@@ -0,0 +1,15 @@
+﻿namespace DotNet7BlazorComponentComms.Services.MessageService
+{
+    public class MessageService
+    {
+        public event Action? SaidSomething;
+
+        public string Message { get; set; } = "...";
+
+        public void SaySomething(string message)
+        {
+            Message = message;
+            SaidSomething?.Invoke();
+        }
+    }
+}
